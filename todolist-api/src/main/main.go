5434e4db3962ec9f9f8c80eb6875eb25085fc9db
Paulo Paula <paulo.almeida.paula@gmail.com>
2020-08-03 21:32:47 +0100

    REST api crud operations done, now i need to adapt to todolist app

--- todolist-api/src/main/main.go
+++ todolist-api/src/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"net/http"
 	"encoding/json"
 	"github.com/gorilla/mux"
@@ -24,11 +25,106 @@ var posts []Post = []Post{}
 
 
 func main() {
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/add", addItem).Methods("POST")
+	router.HandleFunc("/posts", addItem).Methods("POST")
+
+	router.HandleFunc("/posts", getAllPosts).Methods("GET")
+
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+
+	router.HandleFunc("/posts/{id}", patchPost).Methods("PATCH")
 
 	http.ListenAndServe(":5000", router)
+
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+
+	//get the id of the post from the route parameter
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		// there is an error
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer."))
+		return
+	}
+
+	//error checking
+
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified ID"))
+		return
+	}
+
+	post := posts[id]
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified ID"))
+		return
+	}
+
+	//get the value from the JSON body
+
+	var updatedPost Post
+	json.NewDecoder(r.Body).Decode(&updatedPost)
+
+	posts[id] = updatedPost
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
+}
+
+func patchPost(w http.ResponseWriter, r *http.Request) {
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer"))
+		return
+	}
+
+	if id >= len(posts) {
+		w.WriteHeader(404)
+		w.Write([]byte("No post found with specified ID"))
+		return
+	}
+
+	post := posts[id]
+	json.NewDecoder(r.Body).Decode(&post)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(post)
+}
+
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
 
 }
 
@@ -42,7 +138,6 @@ func addItem(w http.ResponseWriter, r *http.Request) {
 	posts = append(posts, newPost)
 
 	w.Header().Set("Content-Type", "application/json")
-
 	json.NewEncoder(w).Encode(posts)
 
 }
