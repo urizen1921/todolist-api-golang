e615da0a637ab54b958460589e7d9dbd533752ce
Paulo Paula <paulo.almeida.paula@gmail.com>
2020-08-04 22:25:49 +0100

    api is done and working, need to tweek a few things regarding cors

--- todolist-api/src/main/main.go
+++ todolist-api/src/main/main.go
@@ -5,139 +5,210 @@ import (
 	"net/http"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/rs/cors"
 )
 
-//User is a struct that represents a user
-type User struct {
-	FullName string `json:"fullName"`
-	UserName string `json:"userName"`
-	Email string `json:"email"`
+//Task is a struct that represents a single task
+type Task struct {
+	gorm.Model
+	Task string `json:"task"`
+	IsComplete bool `json:"isComplete"`
 }
 
-//Post is a struct that represents a single post
-type Post struct {
-	Title string `json:"title"`
-	Body string `json:"body"`
-	Author User `json:"author"`
-}
 
-var posts []Post = []Post{}
+func initialMigration() {
+
+
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Task{})
+}
 
 
 func main() {
 
+	initialMigration()
+
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", addItem).Methods("POST")
+	router.HandleFunc("/tasks", addTask).Methods("POST")
 
-	router.HandleFunc("/posts", getAllPosts).Methods("GET")
+	router.HandleFunc("/tasks", getAllTasks).Methods("GET")
 
-	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
 
-	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/tasks", updateTask).Methods("PUT")
 
-	router.HandleFunc("/posts/{id}", patchPost).Methods("PATCH")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 
-	http.ListenAndServe(":5000", router)
+	c := cors.New(cors.Options{
+	AllowedOrigins: []string{"http://localhost:3001"},
+	AllowCredentials: true,
+	})
+
+	handler := c.Handler(router)
+
+	http.ListenAndServe(":5000", handler)
 
 }
 
-func getPost(w http.ResponseWriter, r *http.Request) {
+func setupResponse(w *http.ResponseWriter, r *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
 
 	//get the id of the post from the route parameter
 	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
+	id, error := strconv.Atoi(idParam)
 
-	if err != nil {
-		// there is an error
+	if error != nil {
 		w.WriteHeader(400)
 		w.Write([]byte("ID could not be converted to integer."))
 		return
 	}
 
-	//error checking
-
-	if id >= len(posts) {
-		w.WriteHeader(404)
-		w.Write([]byte("No post found with specified ID"))
-		return
-	}
-
-	post := posts[id]
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
-
-}
-
-func updatePost(w http.ResponseWriter, r *http.Request) {
-
-	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte("ID could not be converted to integer"))
-		return
+		panic("failed to connect database")
 	}
 
-	if id >= len(posts) {
+	defer db.Close()
+
+	var task Task
+	db.First(&task, id)
+
+	if task.ID == 0 {
 		w.WriteHeader(404)
-		w.Write([]byte("No post found with specified ID"))
+		w.Write([]byte("No task found with specified ID"))
 		return
 	}
 
-	//get the value from the JSON body
-
-	var updatedPost Post
-	json.NewDecoder(r.Body).Decode(&updatedPost)
-
-	posts[id] = updatedPost
+	fmt.Println("{}", task)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(updatedPost)
+	json.NewEncoder(w).Encode(task)
+
 }
 
-func patchPost(w http.ResponseWriter, r *http.Request) {
-	var idParam string = mux.Vars(r)["id"]
-	id, err := strconv.Atoi(idParam)
+func updateTask(w http.ResponseWriter, r *http.Request) {
+
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
-		w.WriteHeader(400)
-		w.Write([]byte("ID could not be converted to integer"))
-		return
+		panic("failed to connect database")
 	}
 
-	if id >= len(posts) {
-		w.WriteHeader(404)
-		w.Write([]byte("No post found with specified ID"))
-		return
-	}
-
-	post := &posts[id]
-	json.NewDecoder(r.Body).Decode(post)
+	var updatedTask Task
+	json.NewDecoder(r.Body).Decode(&updatedTask)
+	db.Save(&updatedTask)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(post)
-}
-
-func getAllPosts(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(posts)
+	json.NewEncoder(w).Encode(updatedTask)
 
 }
 
+func getAllTasks(w http.ResponseWriter, r *http.Request) {
 
-func addItem(w http.ResponseWriter, r *http.Request) {
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	defer db.Close()
+
+	var tasks []Task
+	db.Find(&tasks)
+	fmt.Println("{}", tasks)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("failed to connect to the database")
+	}
+
+	defer db.Close()
 
 	//get Item value from the JSON body
-	var newPost Post
-	json.NewDecoder(r.Body).Decode(&newPost)
+	var newTask Task
+	json.NewDecoder(r.Body).Decode(&newTask)
 
-	posts = append(posts, newPost)
+	fmt.Println(newTask.Task);
+
+	db.Create(&Task{
+		Task: newTask.Task,
+		IsComplete: newTask.IsComplete})
+	fmt.Fprintf(w, "New Task Successfully Created")
+
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(posts)
+	json.NewEncoder(w).Encode(newTask)
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
+
+	db, err := gorm.Open("mysql", "urizen1921:1921urizen@tcp(127.0.0.1:3306)/tasktestgolang?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	defer db.Close()
+
+
+
+	var idParam string = mux.Vars(r)["id"]
+	fmt.Println(mux.Vars(r))
+	id, er := strconv.Atoi(idParam)
+
+	if er != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID could not be converted to integer."))
+		return
+	}
+
+	var task Task
+	db.First(&task, id)
+
+	if task.ID == 0 {
+		w.WriteHeader(404)
+		w.Write([]byte("No task found with specified ID"))
+		return
+	}
+
+	db.Delete(&task)
+
+	fmt.Fprintf(w, "Successfully Deleted Task")
+
+	w.WriteHeader(200)
 
 }
